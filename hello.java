354955bdfd8d29b92b746505d484ff67ea3e39bf
NiraliBaldaniya <niralibaldaniya17@gmail.com>
2026-01-02 17:48:43 +0530

    second commit

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 
 // This is a simple Java program
 
-public class Main {
+public class hello {
     // Entry point of any Java program
     public static void main(String[] args) {
         // To print hello world
