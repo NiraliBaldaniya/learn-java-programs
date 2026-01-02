521360a5a36d1dcf01232d4d6aa71c0ea5e9c88d
NiraliBaldaniya <niralibaldaniya17@gmail.com>
2026-01-02 17:41:29 +0530

    add second commit

--- armstrongnumber.java
+++ armstrongnumber.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class armstrongnumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter armstrong number");
