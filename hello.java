853af7243e026522f7ff1d342158ac32bd5f7a10
NiraliBaldaniya <niralibaldaniya17@gmail.com>
2026-01-02 18:46:27 +0530

    simple hello world program

---
+++ hello.java
@@ -0,0 +1,12 @@
+// This is a simple Java program
+
+public class hello{
+    // Entry point of any Java program
+    public static void main(String[] args) {
+        // To print hello world
+        System.out.println("Hello World");
+    }
+}
+
+// Output:
+// Hello World
