d844dd413281e1cdc5e8a6d6e44cbc470c599ee3
NiraliBaldaniya <niralibaldaniya17@gmail.com>
2026-01-04 15:23:49 +0530

    add fibonacci series

---
+++ java/vscode/fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of element : ");
+        int n = scanner.nextInt();
+
+        int a = 0, b = 1;
+        System.out.println("Fibonacci Series:");
+        for (int i = 1; i <= n; i++)
+        {
+            int next = a + b;
+            a = b;
+            b = next;
+            System.out.print(next+" ");
+        }
+    }
+}
