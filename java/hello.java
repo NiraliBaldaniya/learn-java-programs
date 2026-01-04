ae96d51fc193f19af099d7a27fac289ead3a2f24
NiraliBaldaniya <niralibaldaniya17@gmail.com>
2026-01-04 16:08:04 +0530

    Restore all local files

---
+++ java/hello.java
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+
+=======
+>>>>>>> cbd69890aa3158873a95551fdd7120528c432877
+// This is a simple Java program
+
+public class hello {
+    // Entry point of any Java program
+    public static void main(String[] args) {
+        // To print hello world
+        System.out.println("Hello World");
+    }
+}
+
+// Output:
+// Hello World
+<<<<<<< HEAD
+
+=======
+>>>>>>> cbd69890aa3158873a95551fdd7120528c432877
